f6c6fdc61bc6d4aa8567c548c6f14ef05a843021
luki120 <luki120@exerra.xyz>
2022-03-08 03:00:27 -0300

    removed redundant headers

--- ZiraContentModuleVC.h
+++ ZiraContentModuleVC.h
@@ -1,8 +1,4 @@
-#import <ControlCenterUI/ControlCenterUI-Structs.h>
 #import <ControlCenterUIKit/CCUIButtonModuleViewController.h>
-#import <ControlCenterUIKit/CCUIToggleModule.h>
-#import <ControlCenterUIKit/CCUIButtonModuleView.h>
-#import <ControlCenterUIKit/CCUIContentModuleContentViewController.h>
 #import "GcUniversal/GcImagePickerUtils.h"
 #import "Common.h"
 
