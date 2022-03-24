9e79e99a5eeee9ad6ae743b5906e2025cfbd3916
luki120 <luki120@exerra.xyz>
2022-03-23 21:45:30 -0300

    not needed

--- ZiraModule.h
+++ ZiraModule.h
@@ -1,7 +1,6 @@
 #import "ZiraContentModuleVC.h"
 #import <ControlCenterUIKit/CCUIToggleModule.h>
 #import <ControlCenterUI/ControlCenterUI-Structs.h>
-@import UIKit;
 
 
 @interface ZiraModule : CCUIToggleModule <CCUIContentModule>
